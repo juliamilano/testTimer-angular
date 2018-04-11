9153253876cf0f21daadbc47436c8f7fd55e44cd
juliamilano <t_yulya97@mail.ru>
2018-04-11 22:19:47 +0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,44 +12,79 @@ export class AppComponent {
   hours = '00';
   minutes = '00';
   seconds = '00';
-
-  disablePause = true;
+  staticTime = 0;
+  startStop = "Start";
+  color = "green";
+  milisec;
+  // disablePause = true;
   pause = false;
+
+  valueTimeSave;
 
   sourceSec;
   subscribeSec;
 
-
   startTimerSec(){
-    this.disablePause = false;
-    this.sourceSec = timer(0, 1000);
-    this.subscribeSec = this.sourceSec.subscribe(val => {
-      // if(this.pause){
-      //   val = this.seconds;
-      //   this.pause = false;
-      // }
-      if(val<10){
-        this.seconds= "0" +val;
-      }else if(val>=10 && val<60){
-        this.seconds = val;
-      }else{
-        this.subscribeSec.unsubscribe();
-        this.startTimerSec();
-        this.startTimerMinute();
-    }});
+    // this.disablePause = false;
+    if(!this.pause){
+      this.pause = true;
+      this.startStop = "Stop";
+      this.color = "red";
+      this.sourceSec = timer(0, 1000);
+      this.subscribeSec = this.sourceSec.subscribe(val => {
+        let sec = this.staticTime + val;
+        let min = Math.floor(sec/60);
+        let hour = Math.floor(min/60);
+        sec %= 60;
+        min %= 60;
+        hour %= 60;
+
+        this.valueTimeSave =  this.staticTime + val;
+
+        if(sec<10){
+          this.seconds = "0" + sec;
+        }else{
+          this.seconds = sec;
+        }
+        // else if(sec >= 10 && sec < 60){
+        //   this.seconds = "" + sec;
+        // }
+        if(min < 10){
+          this.minutes = "0" + min;
+        }else{
+          this.minutes = "" + min;
+        }
+        if(hour < 10){
+          this.hours = "0" + hour;
+        }else{
+          this.hours = "" + hour;
+        }
+      });
+    }else{
+      this.stopTimerSec();
+      this.startStop = "Start";
+      this.color = "green"
+    }
+
   }
-  // pauseTimer(){
-  //   this.subscribeSec.unsubscribe();
-  //   this.pause = true;
-  // }
+  pauseTimer(){
+    this.subscribeSec.unsubscribe();
+    this.staticTime = this.valueTimeSave;
+    this.pause = false;
+    this.startStop = "Start";
+    this.color = "green";
+  }
   stopTimerSec(){
     this.subscribeSec.unsubscribe();
+    this.staticTime = 0;
+    this.valueTimeSave = 0;
+    this.pause = false;
   }
   startTimerMinute(){
     let m = 1;
     if (this.minutes == "00" && m < 9){
       this.minutes= "0" + m;
-    }else if(m>10 && m<60){
+    }else if(m >= 10 && m < 60){
       this.minutes = "" + m;
     }else{
       this.startTimerHours()
@@ -62,7 +97,7 @@ export class AppComponent {
     let h = 1;
     if (this.hours == "00" && h < 9){
       this.hours= "0" + h;
-    }else if(h>10 && h<60){
+    }else if(h>=10 && h<60){
       this.hours = "" + h;
     }else{
       this.hours = '00';
@@ -74,6 +109,10 @@ export class AppComponent {
     this.hours = '00';
     this.minutes = '00';
     this.seconds = '00';
+    this.staticTime = 0;
+    this.pause = false;
+    this.startStop = "Start";
+    this.color = "green";
   }
 
 
